3d184e126e65d75016352f6c33020a592a9f671c
Asya Lebedeva <star@asyaspace.com>
2026-08-14 18:04:51 +0700

    build: add script for toggle between tabs in hero block

--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
+'use strict';
 import './scss/style.scss'
+
+window.addEventListener('DOMContentLoaded', () => {
+    const parentTab = document.querySelector('.tabheader__items'),
+          tabs = document.querySelectorAll('.tabheader__item'),
+          tabcontent = document.querySelectorAll('.tabcontent');
+
+    function hideTabs() {
+        tabcontent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        }); // this function only hide tabs (without any choice)
+
+        tabs.forEach((item) => {
+            item.classList.remove('active');
+        })
+    };
+
+    hideTabs();
+
+    function showTabs(n = 0) {
+        tabcontent[n].classList.add('show', 'fade');
+        tabcontent[n].classList.remove('hide');
+        tabs[n].classList.add('active');
+    };
+
+    showTabs();
+
+    parentTab.addEventListener('click', (event) => {
+        if (event.target && event.target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, index) => {
+                if (event.target === item) { // compare element with target (it is needed to be THE SAME!)
+                    hideTabs();
+                    showTabs(index); // I need to find number of current tub (index)
+                };
+            });
+        }
+    })
+})
